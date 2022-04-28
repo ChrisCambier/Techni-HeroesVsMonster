38eff65f3187f0797d60986fa8b11fa881f761bc
Antoine <tempmailpourpres@gmail.com>
2022-04-28 12:50:59 +0200

    Feature Map

---
+++ Techni-HeroesVsMonster/Map.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Techni_HeroesVsMonster
+{
+    internal class Map
+    {
+        public List<IMonstres> Monstres { get; set; }
+        public IHero Hero { get; set; }
+        public ConsoleUI UI { get; set; }
+        public Object[,] Positions { get; set; }
+
+        public Map(int rows, int cols, IHero hero)
+        {
+            Monstres = new List<IMonstres>();
+            Hero = hero;
+            UI = new ConsoleUI();
+            Positions = new Object[rows, cols];
+        }
+        public void Randomize()
+        {
+            throw new NotImplementedException();
+        }
+        public void Display()
+        {
+            throw new NotImplementedException();
+        }
+        
+
+    }
+}
