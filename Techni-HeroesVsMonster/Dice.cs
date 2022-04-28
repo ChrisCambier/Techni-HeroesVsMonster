8384cbc4205f29fe7ba1a1561f610dfb3509e22e
Florentin Delgrange <del_florentin@hotmail.com>
2022-04-28 11:17:18 +0200

    Dice ILoot Monster Tresor

---
+++ Techni-HeroesVsMonster/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Techni_HeroesVsMonster
+{
+    public class Dice
+    {
+        static int Throw(int Max)
+        { 
+            Random aleatoire = new Random();
+            int resultat = aleatoire.Next(1,Max+1); // Génère un entier compris entre 1 et Max
+
+            return resultat;
+        }
+    }
+}
