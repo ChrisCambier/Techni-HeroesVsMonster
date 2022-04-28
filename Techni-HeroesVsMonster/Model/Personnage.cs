f7b86404794ff09b44f4a7d7e7718bfcc981a82f
DE\studentdot0406 <adriendespi@gmail.com>
2022-04-28 11:42:38 +0200

    Methode Combat Fini competence a part

--- Techni-HeroesVsMonster/Model/Personnage.cs
+++ Techni-HeroesVsMonster/Model/Personnage.cs
@@ -6,45 +6,86 @@ using System.Threading.Tasks;
 
 namespace Techni_HeroesVsMonster.Model
 {
+    
     public class Personnage
     {
-        private int choix = 0;
+        
+       
         private int _Hp = 0;
 
         public int Str {  get; set; }
-        public int Hp { get; set; }
+        public int Hp
+        {
+            get { return _Hp; }
+            set { _Hp = value; }
+        }
+        
         public int End { get; set; }
         public int Intel { get; set; }
-        public string race { get; set; }
         public string Nom { get; set; }
         public string Skin { get; set; }
 
+       
+
 
         
-        private void Combat(Personnage Hero ,Personnage Monster)
+        private bool Combat(Personnage Hero ,Personnage Monster)
         {
-            Console.WriteLine($"{Monstre.Nom} veux te goumer");
-            Console.WriteLine("Choissir une action");
-            Console.WriteLine("1.Frapper\n2.Competence\n3.Fuir");
+            int choix = 0;
+            Boolean Mort=false;
+            Console.WriteLine($"{Monster.Nom} veux te goumer");
 
-            while(choix<1 || choix > 3)
+
+
+            while (!Mort)
             {
-                Console.WriteLine("1.Frapper\n2.Competence\n3.Fuir");
-                int.TryParse(Console.ReadLine(), out choix);
+                while (choix < 1 || choix > 3)
+                    choix = 0;
+                {
+                    Console.WriteLine("Choissir une action");
+                    Console.WriteLine("1.Frapper\n2.Competence\n3.Fuir");
+                    int.TryParse(Console.ReadLine(), out choix);
+                }
+                
+                switch (choix)
+                {
+                    case 1:
+                        Console.WriteLine("Pattate de forain");
+                        Strike(Hero, Monster);
+
+                        break;
+                    case 2:
+                        Skill();
+                        break;
+                    case 3:
+                        Console.WriteLine("vous prenez la fuite");
+                        return Mort;
+                        break;
+
+                }
+
+                Tank(Hero, Monster);
+
+                if (Hero.Hp <= 0 || Monster.Hp <= 0)
+                {
+                    Mort=true;
+                }
             }
-            switch (choix)
-            {
-                case 1:
-                    Strike(Hero.Str);
-                    Tank(Mons)
-                    break;
-            }
-            
-            Strike(Hero, Monstre);
-            
+            return Mort;
             
         }
 
-        
+        public virtual void Tank(Personnage hero, Personnage monster)
+        {
+            hero.Hp -= monster.Str;
+        }
+
+        public void Strike(Personnage hero,Personnage monster)
+        {
+            monster.Hp -= hero.Str;
+        }
+
+        public abstract void Skill();
+       
     }
 }
