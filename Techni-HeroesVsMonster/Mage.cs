70829852bb5892dbbdf14e1f13376f62c4341104
Christopher Cambier <C.Cambier@live.fr>
2022-04-28 10:45:54 +0200

    Ajout Feature Archétype (Barbare, Mage, Paladin)

---
+++ Techni-HeroesVsMonster/Mage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Techni_HeroesVsMonster
+{
+    public class Mage : Personnage
+    {
+        private int _Mana;
+        public int Mana { get; set; }
+        public Mage(int Intel)
+    {
+        Mana = Intel*2;
+    }
+
+        public virtual Fireball(Mage.Intel, Mage.Mana)
+        {
+
+        }
+}
