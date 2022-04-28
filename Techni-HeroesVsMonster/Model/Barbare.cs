17d7de6b54d9e775df20bf307de2a6709a4246aa
Christopher Cambier <C.Cambier@live.fr>
2022-04-28 13:39:59 +0200

    Skill des Archétypes

--- Techni-HeroesVsMonster/Model/Barbare.cs
+++ Techni-HeroesVsMonster/Model/Barbare.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Techni_HeroesVsMonster.Model;
 
 namespace Techni_HeroesVsMonster
 {
     public class Barbare : Personnage
     {
-        public override Strike(int str)
+        public override Skill(Personnage hero, Personnage monster)
         {
-            Console.WriteLine($"");
-        }
-        public override Tank(int hp)
-        {
-            
+            int dégât = 0,
+                retour = 0;
+
+            dégât = hero.Str * 2.5;
+            retour = hero.Str * 0.3;
+            Console.WriteLine($"{hero.Name} gonfle les muscles et frappe comme un enragé. Il inflige {dégât} dégâts et se blesse de {retour} points de vie.");
+            monster.Hp -= dégât;
+            hero.Hp -= retour;
+
         }
     }
 }
--- Techni-HeroesVsMonster/Model/Paladin.cs
+++ Techni-HeroesVsMonster/Model/Paladin.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Techni_HeroesVsMonster.Model;
 
 namespace Techni_HeroesVsMonster
 {
     public class Paladin
     {
-        public override Strike(Hero.Str)
+        public override Skill(Personnage hero, Personnage monster)
         {
+            int dégât = 0,
+                soin  = 0;
 
+            dégât = hero.Str * 0.6;
+            soin = (hero.Str * 0.6)/2;
+            Console.WriteLine($"{hero.Name} voit son épée se gorger de puissance divine. Il inflige {dégât} dégâts et récupère {soin} points de vie.");
+            monster.Hp -= hero.Str * 0.6;
+            if (hero.Hp + soin >= hero._Hp)
+                hero.Hp = hero._Hp;
+            else
+                hero.Hp += soin;
+            
         }
-        public override Tank(Hero.Hp)
+        public override Tank(Personnage hero, Personnage monster)
         {
-
+            int dégât = 0;
+            dégât = monster.Str - 5;
+            if (dégât > 0)
+            {
+                Console.WriteLine($"La lourde armure du Paladin le protège. {hero.Name} subit {dégât} dommage(s).");
+                hero.Hp -= dégât;
+            }
+            else
+            Console.WriteLine($"La lourde armure du Paladin le protège efficacement. {hero.Name} ne subit aucun dégât !");
         }
     }
 }
